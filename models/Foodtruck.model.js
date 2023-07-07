89f61b4f75f542441a93fe35ae50526e1bfd473e
Joao Faria <joaorafaria@gmail.com>
2023-07-07 01:30:36 +0100

    delete enum

--- models/Foodtruck.model.js
+++ models/Foodtruck.model.js
@@ -3,31 +3,14 @@ const { Schema, model } = mongoose;
 
 const truckSchema = new Schema({
   name: String,
-  category: {
-    type: String,
-    enum: [
-      "German",
-      "Portuguese",
-      "French",
-      "Thai",
-      "Burger",
-      "Sushi",
-      "Crêpes",
-      "Pizza",
-      "Spanish",
-      "Tea",
-      "Italian",
-      "Hungarian",
-      "Brasilian",
-      "Vegetarian",
-      "Soulfood"
-    ]},
+  category: String,
   imageUrl: String,
   owner: String,
   events: [{ type: Schema.Types.ObjectId, ref: "Event" }],
   createdBy: {
-    type: Schema.Types.ObjectId, ref: "User",
-  }
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = model("Truck", truckSchema);
